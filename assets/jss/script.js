4d8c1bba90acefa2ff1083827fc5ce88022ae2e5
Alan Phelps <phelpsa64@gmail.com>
2022-01-12 20:52:45 -0500

    searched for artists are stored in localStorage and appended to the page as buttons

--- assets/jss/script.js
+++ assets/jss/script.js
@@ -1,13 +1,38 @@
+const artistsFormEL = document.querySelector('#artistsFormEl');
 const artistSearchInputEl = document.querySelector('#artistName');
+const artistsSimilarToEl = document.querySelector('#artistsSimilarToEl');
+const mostRecentSearchContainerEL = document.querySelector('#mostRecentSearchContainer');
+
+// adds a new artist button when a new search is performed in the page's artist form
+
+function addArtist() {
+    let artistButtonEl = document.createElement ('button');
+    artistButtonEl.textContent = artistSearchInputEl.value;
+    mostRecentSearchContainerEL.appendChild(artistButtonEl);
+
+    // together with the event listener below, this function allows the user to see a just-searched-for artist's information again if they click back to it after clicking on an older searched-for artist's button
+    // function addArtistInformation() {
+    //     let artistName = artistButtonEl.textContent;
+
+    //     getArtistInfo(artistName);
+    // }
+
+    // artistButtonEl.addEventListener('click', getArtistInfo);
+}
 
 // tells the page what to do when the user searches for an artist using the page's form
 function formSubmitHandler(event) {
     event.preventDefault();
 
-    let artistName = artistSearchInputEl.ariaValueMax.trim();
+    let artistName = artistSearchInputEl.value.trim();
 
     if (artistName) {
         localStorage.setItem(JSON.stringify(artistName), JSON.stringify(artistName));
-        // getArtistInfo(artistName);
+        addArtist();
+        artistSearchInputEl.value = '';
+    } else {
+        console.log("Input an artist name");
     }
 }
+
+artistsFormEL.addEventListener('submit', formSubmitHandler);
