31fd4c5e95e0e74659a16414c61d6367572570d2
TCL git <tuckerlarrabee13@gmail.com>
2022-01-13 15:36:01 -0500

    trying to get access token from spotify API, no luck yet

---
+++ assets/jss/script.js
@@ -0,0 +1,156 @@
+const TOKEN = "https://accounts.spotify.com/api/token";
+
+var client_id = '0c243873294b4a90a22830738792f105';
+var client_secret = 'e10f00e4371444eca4ccbc462c5d3a90';
+
+var authOptions = {
+    url: 'https://accounts.spotify.com/api/token',
+    headers: {
+        'Authorization': 'Basic ' + (btoa(client_id + ':' + client_secret)),
+        'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    form: {
+        grant_type: 'client_credentials'
+    },
+    json: true
+};
+
+function requestAuthorization() {
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", TOKEN, true);
+    xhr.send(JSON.stringify(authOptions));
+}
+
+
+
+
+
+
+// var authOptions = {
+//     url: 'https://accounts.spotify.com/api/token',
+//     headers: {
+//         'Authorization': 'Basic ' + (btoa(client_id + ':' + client_secret)),
+//         'Content-Type': 'application/x-www-form-urlencoded'
+//     },
+//     form: {
+//         grant_type: 'client_credentials'
+//     },
+//     json: true
+// };
+
+
+// function requestAuthorization() {
+
+//     var results = fetch(
+//         'https://accounts.spotify.com/api/token',
+//         {
+//             data: {
+//                 grant_type: 'client_credentials',
+//             },
+//             headers: {
+//                 'Content-Type': 'application/x-www-form-urlencoded',
+//                 'Authorization': 'Basic ' + (btoa(client_id + ':' + client_secret))
+//             }
+//         }
+//     );
+//     console.log(results)
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var redirectUri = 'http://127.0.0.1:5500/index.html';
+
+// var clientId = '0c243873294b4a90a22830738792f105';
+// var clientSecret = 'e10f00e4371444eca4ccbc462c5d3a90';
+// var code;
+
+// const AUTHORIZE = "https://accounts.spotify.com/authorize";
+// const TOKEN = "https://accounts.spotify.com/api/token";
+
+// function onPageLoad() {
+//     if (window.location.search.length > 0) {
+//         handleRedirect();
+//     }
+// }
+
+// function handleRedirect() {
+//     code = getCode();
+//     fetchAccessToken(code)
+// }
+
+// function fetchAccessToken(code) {
+//     let body = "grant_type=authorization_code";
+//     body += "&code=" + code;
+//     body += "&redirect_uri=" + encodeURI(redirectUri);
+//     body += "&client_id=" + clientId;
+//     body += "&client_secret=" + clientSecret;
+//     console.log(body)
+//     callAuthorizationApi(body);
+// }
+
+// function callAuthorizationApi(body) {
+//     let xhr = new XMLHttpRequest();
+//     xhr.open("POST", TOKEN, true);
+//     xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+//     xhr.setRequestHeader('Authorization', 'Basic ' + btoa(clientId + ':' + clientSecret));
+//     xhr.send(body);
+//     xhr.onload = handleAuthorizationResponse;
+// }
+
+// function handleAuthorizationResponse() {
+//     if (this.status == 200) {
+//         var data = JSON.parse(this.responseText);
+//         console.log(data);
+//         var data = JSON.parse(this.responseText);
+//         if (data.access_token != undefined) {
+//             access_token = data.access_token;
+//             localStorage.setItem("access_token", access_token);
+//         }
+//         if (data.refresh_token != undefined) {
+//             refresh_token = data.refresh_token;
+//             localStorage.setItem("refresh_token", refresh_token)
+//         }
+//         onPageLoad();
+//     }
+//     else {
+//         console.log(this.responseText);
+//         alert(this.responseText)
+//     }
+// }
+
+// function getCode() {
+//     let code = null
+//     const queryString = window.location.search;
+//     if (queryString.length > 0) {
+//         const urlParams = new URLSearchParams(queryString);
+//         code = urlParams.get('code')
+//         console.log(code)
+//     }
+//     return code;
+// }
+
+// function requestAuthorization(event) {
+//     let url = AUTHORIZE;
+//     url += "?client_id=" + clientId;
+//     url += "&response_type=code";
+//     url += "&redirect_uri=" + encodeURI(redirectUri)
+//     console.log(url)
+//     window.location.href = url
+// }
+
+
+
